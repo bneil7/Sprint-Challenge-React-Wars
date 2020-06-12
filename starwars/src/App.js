378692fe8b40be0bf4de3ff3488602550a53fd25
Brandon Neil <brandonneil@Brandons-MacBook-Pro.local>
2020-06-12 10:01:23 -0600

    set up base, API is returning array of characters in console log

--- starwars/src/App.js
+++ starwars/src/App.js
@@ -1,5 +1,7 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import './App.css';
+import {BASE_URL} from './constants/index';
+import axios from 'axios';
 
 const App = () => {
   // Try to think through what state you'll need for this app before starting. Then build out
@@ -8,6 +10,21 @@ const App = () => {
   // Fetch characters from the API in an effect hook. Remember, anytime you have a 
   // side effect in a component, you want to think about which state and/or props it should
   // sync up with, if any.
+
+  const [swData, setSwData] = useState([])
+
+  useEffect(() => {
+    axios
+      .get(`${BASE_URL}`)
+      .then(res => {
+        console.log('AW YEAH', res.data)
+        setSwData(res.data)
+       })
+       .catch(error => {
+       debugger
+       console.log('OH NO', error)
+       })
+   }, []);
 
   return (
     <div className="App">
